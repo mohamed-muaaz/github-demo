b1602046248fd6e4d200ac877f852eca6209df5f
mohamed-muaaz <80963709+mohamed-muaaz@users.noreply.github.com>
2021-03-22 15:49:37 +0530

    Add functional implementations to math.py

--- math.py
+++ math.py
@@ -1,1 +1,23 @@
+# Add implementation
+def add(x,y):
+    return x+y
+
+# Substract implementation
+def subtract(x,y):
+    return x+y
+
+# Multiply implementation
+def multiply(x,y):
+    pass
+
+# Division implementation
+def divide(x,y):
+    if y==0:
+        return "Error"
+    else:
+        return x/y
+
+# Square implementation
+def square(x):
+    return x*x
 
