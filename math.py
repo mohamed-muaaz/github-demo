3642f50e9ee49963ad04d8481488b31005744a6f
mohamed_muaaz <mohamed_muaaz@persistent.com>
2021-03-22 16:41:40 +0530

    Add function headers to math.py

--- math.py
+++ math.py
@@ -1,15 +1,20 @@
+# Function to add two numbers
 def add(x,y):
     pass
 
+# Function to substract a number from a number
 def subtract(x,y):
     pass
 
+# Function to multiply two numbers
 def multiply(x,y):
     pass
 
+# Function to devide a number by another number
 def divide(x,y):
     pass
 
+# Function to square a number
 def square(x):
     pass
 
